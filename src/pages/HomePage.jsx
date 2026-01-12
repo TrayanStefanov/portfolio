d6f0868ca415eb26f55fcc8264dda53659c20b2a
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-12 17:39:14 +0200

    HomePage v0.2.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import ProfileImage from "../components/ProfileImage";
+import TechStack from "../components/TechStack";
+import AboutSection from "../components/AboutSection";
+import LatestProjects from "../components/LatestProjects";
+import IntroSection from "../components/IntroSection";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="overflow-hidden pt-20 min-h-[80vh] mx-8 text-center justify-items-center">
+      <IntroSection />
+      <ProfileImage />
+      <TechStack />
+      <AboutSection />
+      <LatestProjects />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
