d8cf6fdf02b3ba52b9fc71b7acd5b9d7efa5b02e
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-12 17:38:17 +0200

    ProfileImage v0.1. Initial commit.

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion";
+
+const ProfileImage = () => {
+  return (
+    <div className="flex items-center justify-center my-12">
+      {/* Left Arrow */}
+      {/* <div className="h-46 text-secondary opacity-80 hover:opacity-100 content-start cursor-default text-6xl/4 ">
+        {"<"}
+      </div> */}
+
+      {/* Halo Container */}
+      <div className="relative flex items-center justify-center">
+        <motion.div
+          /* animate={{ scale: [1, 1.3, 1, 0.8, 1] }}
+          transition={{ repeat: Infinity, duration: 5, ease: "easeInOut" }} */
+          className="absolute -left-[13px] -top-4 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-[50px]/11"
+        >
+          {"<"}
+        </motion.div>
+        <motion.div
+          animate={{ x: [-8, 0, -8] }}
+          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          className="absolute -left-12 -top-14 text-secondary text-[160px] font-thin select-none"
+        >
+          {"{"}
+        </motion.div>
+        {/* Profile Image */}
+        <div className="relative w-46 h-46 content-end rounded-full border-2 border-secondary bg-radial from-accent from-40% to-primary to-100% shadow-xl">
+          <img
+            src="/profile.png" // <-- replace with your image path
+            alt="Profile"
+            className="w-32 h-44 justify-self-center mt-2 object-cover"
+          />
+        </div>
+        <motion.div
+          animate={{ x: [8, 0, 8] }}
+          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          className="absolute -right-12 -top-14 text-secondary text-[160px] font-thin select-none"
+        >
+          {"}"}
+        </motion.div>
+        <motion.div
+          /* animate={{ scale: [1, 1.1, 1, 0.9, 1] }}
+          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }} */
+          className="absolute -right-4 bottom-0 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11"
+        >
+          {">"}
+        </motion.div>
+      </div>
+
+      {/* Right Arrow */}
+      {/* <div className="h-46 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11">
+        {">"}
+      </div> */}
+    </div>
+  );
+};
+
+export default ProfileImage;
