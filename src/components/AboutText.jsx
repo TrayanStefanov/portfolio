64d3429ce85b2ede978c9c8b2a14a597b39a7d63
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-16 17:47:44 +0200

    About page v0.1. Initial commit.

---
+++ src/components/AboutText.jsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
+
+const AboutText = () => {
+  const { t } = useTranslation();
+
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+      className="max-w-4xl mx-4  mt-8 text-center text-secondary justify-self-center"
+    >
+      <h1 className="text-3xl md:text-5xl font-bold text-center mb-4 lg:mb-8 underline underline-offset-8 decoration-1 decoration-secondary">
+            {t("about.title")}
+          </h1>
+          <p className="text-xl lg:text-2xl mb-6 text-secondary/70">{t("about.subtitle")}</p>
+
+      <p className="text-base-200 mx-6 text-base md:text-lg leading-relaxed">
+        {t("about.part1")}
+      </p>
+      <p className="text-base-200 mx-6 my-8 text-base md:text-lg leading-relaxed">
+        {t("about.part2")}
+      </p>
+    </motion.section>
+  );
+};
+
+export default AboutText;
