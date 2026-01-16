81e25c760bfa5701f995538919db2c8ac2ee682c
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-16 17:49:33 +0200

    About Page v0.2. Desktop version.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,18 @@
-import TechCloud from "../components/TechCloud"
+import TechCloud from "../components/TechCloud";
+import ProfileImage from "../components/ProfileImage";
+import AboutText from "../components/AboutText";
+import ExperienceText from "../components/ExperienceText";
 const AboutPage = () => {
   return (
-    <div className="mt-[4rem]">
-      <TechCloud />
+    <div className="my-[4rem]">
+      <ProfileImage />
+      <div className="border-t-2 border-b-2 border-secondary/70">
+        <TechCloud />
+      </div>
+      <AboutText />
+      <ExperienceText />
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
