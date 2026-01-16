171a0d6596a6250e42ca008619efe6519947c6c3
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-16 17:56:12 +0200

    StepCard v0.2. Desktop version.

--- src/components/StepCard.jsx
+++ src/components/StepCard.jsx
@@ -1,11 +1,15 @@
+import { useTranslation } from "react-i18next";
+
 const StepCard = ({ step, index }) => {
+  const { t } = useTranslation();
+  const direction = index % 2 === 0 ? "flex-row" : "flex-row-reverse";
   return (
-    <div className="grid md:grid-cols-2 gap-8 items-start mb-10 mx-8">
+    <div className="items-start mb-10 mx-8">
       {/* Text */}
-      <div>
+      <div className={`flex gap-10 ${direction}`}>
         {/* Image (optional) */}
         {step.image && (
-          <div className="rounded overflow-hidden border border-white/10">
+          <div className="rounded overflow-hidden max-w-[50%]">
             <img
               src={step.image}
               alt={step.title}
@@ -14,26 +18,30 @@ const StepCard = ({ step, index }) => {
             />
           </div>
         )}
-        <h3 className="text-xl font-semibold text-base-200 my-4">
-          <span className="text-secondary mr-2">
-            {String(index + 1)}.
-          </span>
-          {step.title}
-        </h3>
+        <div className="flex flex-col">
+          <h3 className="text-xl lg:text-3xl font-semibold text-base-200 my-4">
+            <span className="text-secondary mr-2">{String(index + 1)}.</span>
+            {step.title}
+          </h3>
 
-        <p className="indent-4 text-base-300 text-sm leading-relaxed">{step.content}</p>
+          <p className="indent-4 text-base-300 text-sm lg:text-xl leading-relaxed">
+            {step.content}
+          </p>
 
-        {/* Notes (optional) */}
-        {step.notes && (
-          <div className="mt-4 border-l-2 border-accent pl-4">
-            <h4 className="text-sm uppercase text-secondary mb-2">Notes</h4>
-            <ul className="indent-2 text-sm text-gray-400 space-y-1">
-              {step.notes.map((note, i) => (
-                <li key={i}><span className="text-secondary">-</span> {note}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+          {/* Notes (optional) */}
+          {step.notes && (
+            <div className="mt-4 border-l-2 border-accent pl-4">
+              <h4 className="text-sm lg:text-lg uppercase text-secondary mb-2">{t("projects.caseStudy.notes")}</h4>
+              <ul className="indent-2 text-sm lg:text-lg text-base-300 space-y-1">
+                {step.notes.map((note, i) => (
+                  <li key={i}>
+                    <span className="text-secondary">-</span> {note}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
