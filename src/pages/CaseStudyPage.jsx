316a73ee717cc777f5b37916f601096e1e90bcc3
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-25 18:29:25 +0200

    CaseStudy v0.2.1 Desktop version polish

--- src/pages/CaseStudyPage.jsx
+++ src/pages/CaseStudyPage.jsx
@@ -22,57 +22,66 @@ const CaseStudyPage = () => {
   }
 
   return (
-    <section className="max-w-6xl rounded-xs mx-auto mt-[4rem] lg:mt-[8rem] px-4 py-16">
+    <section className="max-w-6xl rounded-xs mx-auto mt-[2rem] lg:mt-[8rem] px-4 py-16">
       {/* Intro */}
-      <header className="mb-8 mx-4 grid lg:grid-cols-2 gap-10 items-center">
+      <header className="mb-8 mx-4 flex flex-col gap-10 items-center">
+        <h1 className="hidden lg:block text-3xl md:text-5xl font-bold text-secondary text-center mb-4">
+          {project.title}
+        </h1>
         <div>
+          {project.mainImage && (
+            <img
+              src={project.mainImage}
+              alt={project.title}
+              className="hidden lg:block rounded border border-white/10"
+            />
+          )}
+          {project.links && (
+            <div className="hidden lg:flex flex-wrap justify-center gap-4 mt-10">
+              {project.links.live && (
+                <a
+                  href={project.links.live}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-accent rounded-none text-secondary px-6"
+                >
+                  {t("projects.caseStudy.links.live")}
+                </a>
+              )}
 
-        {project.mainImage && (
-          <img
-          src={project.mainImage}
-            alt={project.title}
-            className="hidden lg:block rounded border border-white/10"
-          />
-        )}
-        {project.links && (
-          <div className="flex flex-wrap justify-center gap-4 mt-10">
-            {project.links.live && (
-              <a
-                href={project.links.live}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-accent rounded-none text-secondary px-6"
-              >
-                {t("projects.caseStudy.links.live")}
-              </a>
-            )}
+              {project.links.demo && (
+                <a
+                  href={project.links.demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-accent rounded-none text-secondary px-6"
+                >
+                  {t("projects.caseStudy.links.demo")}
+                </a>
+              )}
 
-            {project.links.demo && (
-              <a
-                href={project.links.demo}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-accent rounded-none text-secondary px-6"
-              >
-                {t("projects.caseStudy.links.demo")}
-              </a>
-            )}
-
-            {project.links.github && (
-              <a
-                href={project.links.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-accent rounded-none text-secondary px-6"
-              >
-                {t("projects.caseStudy.links.github")}
-              </a>
-            )}
-          </div>
-        )}
+              {project.links.github && (
+                <a
+                  href={project.links.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-accent rounded-none text-secondary px-6"
+                >
+                  {t("projects.caseStudy.links.github")}
+                </a>
+              )}
+            </div>
+          )}
         </div>
         <div>
-          <h1 className="text-3xl md:text-5xl font-bold text-base-200 text-center mb-4">
+          {project.mainImage && (
+            <img
+              src={project.mainImage}
+              alt={project.title}
+              className="rounded border lg:hidden mb-4 border-white/10"
+            />
+          )}
+          <h1 className="lg:hidden text-3xl md:text-5xl font-bold text-base-200 text-center mb-4">
             {project.title}
           </h1>
           <p className="hidden lg:block text-base-300 text-lg lg:text-2xl mb-6">
@@ -99,20 +108,20 @@ const CaseStudyPage = () => {
             )}
           </div>
           {project.tech && project.tech.length > 0 && (
-        <section className="hidden lg:block mb-6 mx-10">
-          <h2 className="text-2xl font-semibold text-white mb-6 text-center">
-            {t("projects.caseStudy.tech")}
-          </h2>
-          <TechStack tech={project.tech} />
-        </section>
-      )}
+            <section className="hidden lg:block mb-6 mx-10">
+              <h2 className="text-2xl font-semibold text-white mb-6 text-center">
+                {t("projects.caseStudy.tech")}
+              </h2>
+              <TechStack tech={project.tech} />
+            </section>
+          )}
         </div>
-        <p className="block lg:hidden text-base-300 text-xl mb-6">
+        <p className="block lg:hidden text-base-300 text-lg mb-6">
           {project.intro}
         </p>
 
         {/* Meta */}
-        <div className="lg:hidden text-base text-base-300">
+        <div className="lg:hidden w-full text-base text-base-300 text-left">
           <p>
             <strong>
               {t("projects.caseStudy.category")}
@@ -134,7 +143,7 @@ const CaseStudyPage = () => {
         </div>
         {/* CTA buttons */}
         {project.links && (
-          <div className="lg:hidden flex flex-wrap justify-center gap-4 mb-2">
+          <div className="lg:hidden flex  flex-wrap justify-center gap-4 mb-2">
             {project.links.live && (
               <a
                 href={project.links.live}
@@ -170,13 +179,13 @@ const CaseStudyPage = () => {
           </div>
         )}
 
-        {project.mainImage && (
+        {/* {project.mainImage && (
           <img
             src={project.mainImage}
             alt={project.title}
             className="rounded border lg:hidden border-white/10"
           />
-        )}
+        )} */}
       </header>
 
       {/* Tech Stack */}
