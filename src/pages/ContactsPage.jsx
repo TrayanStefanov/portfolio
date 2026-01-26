393e6806743efb9a904243893c8c594839ce78e6
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-26 18:36:22 +0200

    ContactsPage v0.2.1. Style update.

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -12,21 +12,27 @@ const ContactsPage = () => {
   const email = `${emailUser1}${emailUser2}@${emailDomain}`;
   return (
     <div className="overflow-hidden pt-20 min-h-[80vh] mx-15 lg:mt-15 text-center justify-items-center lg:gap-y-16 content-center">
-      <h1 className="hidden text-3xl lg:text-4xl text-base-200 lg:flex flex-col lg:flex-row lg:gap-2 lg:mb-8">
-        <span className=" decoration-2 underline underline-offset-8 decoration-secondary">
+      <h1 className="hidden text-3xl lg:text-5xl text-base-200 lg:flex flex-col lg:flex-row lg:gap-2 lg:mb-8">
+        <span>
           {t("contacts.desktopTitle.value1")}
         </span>
-        <span className="text-secondary">?</span>
-      </h1>
-      <h1 className="hidden text-3xl lg:text-4xl text-base-200 lg:flex flex-col lg:flex-row lg:gap-2 lg:mb-6">
-        <span className=" decoration-2 underline underline-offset-8 decoration-secondary">
+        <span className="text-secondary font-semibold">
           {t("contacts.desktopTitle.value2")}
         </span>
         <span className="text-secondary">?</span>
       </h1>
-      <h1 className="text-3xl lg:text-4xl text-base-200 flex flex-col lg:flex-row lg:gap-2 lg:mb-16">
+      <h1 className="hidden text-3xl lg:text-5xl text-base-200 lg:flex flex-col lg:flex-row lg:gap-2 lg:mb-6">
+        <span>
+          {t("contacts.desktopTitle.value3")}
+        </span>
+        <span className="text-secondary font-semibold">
+          {t("contacts.desktopTitle.value4")}
+        </span>
+        <span className="text-secondary">?</span>
+      </h1>
+      <h1 className="text-3xl lg:text-5xl text-base-200 flex flex-col lg:flex-row lg:gap-2 lg:mb-16">
         {t("contacts.title.value1")}
-        <span className="decoration-2 underline underline-offset-8 decoration-secondary">
+        <span className="text-secondary font-semibold">
           {t("contacts.title.value2")}
         </span>
         <span>
@@ -42,10 +48,10 @@ const ContactsPage = () => {
                 <FiPhoneCall className="w-6 h-6 lg:h-10 lg:w-10 self-center text-secondary" />
               </div>
               <div className="flex flex-col">
-                <span className="text-xs lg:text-2xl text-base-200 text-left">
+                <span className="text-xs md:text-xl lg:text-3xl text-base-200 text-left">
                   {t("contacts.phoneLabel")}
                 </span>
-                <span className="text-[10px] lg:text-xl text-base-300 indent-1.5 text-left -mt-0.5">
+                <span className="text-[10px] md:text-lg lg:text-2xl text-secondary indent-1.5 text-left -mt-0.5 font-semibold">
                   {t("contacts.phone")}
                 </span>
               </div>
@@ -55,10 +61,10 @@ const ContactsPage = () => {
                 <FiMail className="w-6 h-6 lg:h-10 lg:w-10 self-center text-secondary" />
               </div>
               <div className="flex flex-col">
-                <span className="text-xs lg:text-2xl text-base-200 text-left">
+                <span className="text-xs md:text-xl lg:text-3xl text-base-200 text-left">
                   {t("contacts.emailLabel")}
                 </span>
-                <span className="text-[10px] lg:text-xl text-base-300 indent-1.5 text-left -mt-0.5">
+                <span className="text-[10px] md:text-lg lg:text-2xl text-secondary indent-1.5 text-left -mt-0.5 font-semibold">
                   {email}
                 </span>
               </div>
@@ -70,10 +76,10 @@ const ContactsPage = () => {
                 <TbBrandLinkedin className="w-6 h-6 lg:h-10 lg:w-10 self-center text-secondary" />
               </div>
               <div className="flex flex-col">
-                <span className="text-xs lg:text-2xl text-base-200 text-left">
+                <span className="text-xs md:text-xl lg:text-3xl text-base-200 text-left">
                   {t("contacts.linkedinLabel")}
                 </span>
-                <span className="text-[10px] lg:text-xl text-base-300 indent-1.5 text-left -mt-0.5">
+                <span className="text-[10px] md:text-lg lg:text-2xl text-secondary indent-1.5 text-left -mt-0.5 font-semibold">
                   {t("contacts.linkedin")}
                 </span>
               </div>
@@ -83,25 +89,25 @@ const ContactsPage = () => {
                 <FaViber className="w-6 h-6 lg:h-10 lg:w-10 self-center text-secondary" />
               </div>
               <div className="flex flex-col">
-                <span className="text-xs lg:text-2xl text-base-200 text-left">
+                <span className="text-xs md:text-xl lg:text-3xl text-base-200 text-left">
                   {t("contacts.viberLabel")}
                 </span>
-                <span className="text-[10px] lg:text-xl text-base-300 indent-1.5 text-left -mt-0.5">
+                <span className="text-[10px] md:text-lg lg:text-2xl text-secondary indent-1.5 text-left -mt-0.5 font-semibold">
                   {t("contacts.viber")}
                 </span>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-row gap-5 lg:self-center">
+        <div className="flex flex-row mt-4 gap-5 lg:self-center">
           <div className="self-center">
             <TbBrandGithub className="w-6 h-6 lg:h-10 lg:w-10 self-center text-secondary" />
           </div>
           <div className="flex flex-col">
-            <span className="text-xs lg:text-2xl text-base-200 text-left">
+            <span className="text-xs md:text-xl lg:text-3xl text-base-200 text-left">
               {t("contacts.githubLabel")}
             </span>
-            <span className="text-[10px] lg:text-xl text-base-300 indent-1.5 text-left -mt-0.5">
+            <span className="text-[10px] md:text-lg lg:text-2xl indent-1.5 text-left -mt-0.5 text-secondary font-semibold">
               {t("contacts.github")}
             </span>
           </div>
