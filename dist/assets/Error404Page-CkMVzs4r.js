e1bea655caaeea6d00d191f4477d4040df1686f6
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-27 14:58:43 +0200

    Dist 0.7.

---
+++ dist/assets/Error404Page-CkMVzs4r.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-rfWGvdZI.js";const e=()=>r.jsx("div",{children:"404"});export{e as default};
--- dist/assets/ProjectCard-fKL6rbmZ.js
+++ dist/assets/ProjectCard-fKL6rbmZ.js
@@ -1,4 +1,4 @@
-import{a as r,u as i,j as t,l as c,n as d,o as x,p as m,q as u,s as g,t as h}from"./index-W-dFZy1G.js";import{S as p}from"./index-BaUEwwlX.js";const b={HTML:h,CSS:g,JavaScript:u,"Node.js":m,React:x,Tailwind:p,Git:d,GitHub:c},N=({project:s})=>{const l=r(),{t:o}=i(),a=()=>{l(`/projects/${s.slug||"coming-soon"}`)};return t.jsxs("div",{role:"button",tabIndex:0,onClick:a,onKeyDown:e=>e.key==="Enter"&&a(),className:`\r
+import{a as r,u as i,j as t,l as c,n as d,o as x,p as m,q as u,s as g,t as h}from"./index-rfWGvdZI.js";import{S as p}from"./index-DULEm8re.js";const b={HTML:h,CSS:g,JavaScript:u,"Node.js":m,React:x,Tailwind:p,Git:d,GitHub:c},N=({project:s})=>{const l=r(),{t:o}=i(),a=()=>{l(`/projects/${s.slug||"coming-soon"}`)};return t.jsxs("div",{role:"button",tabIndex:0,onClick:a,onKeyDown:e=>e.key==="Enter"&&a(),className:`\r
         cursor-pointer overflow-hidden\r
         border border-white/5\r
         shadow-md hover:shadow-lg\r
