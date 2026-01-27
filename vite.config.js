240b1b39094d99f5768d19daee3dd9af6c6a5077
TrayanStefanov <trayanstefanov95@gmail.com>
2026-01-27 13:36:45 +0200

    Assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Google Sans Flex"', 'ui-sans-serif', 'system-ui'],
+        google: ['"Google Sans Flex"', 'sans-serif'],
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
