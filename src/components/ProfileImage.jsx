df251bae3d8b740cc9793223d6779953db6bcf64
TrayanStefanov <trayanstefanov95@gmail.com>
2026-02-15 18:22:03 +0200

    Dist 9.1.

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -13,36 +13,36 @@ const ProfileImage = () => {
         <motion.div
           /* animate={{ scale: [1, 1.3, 1, 0.8, 1] }}
           transition={{ repeat: Infinity, duration: 5, ease: "easeInOut" }} */
-          className="absolute -left-3 -top-4 lg:-left-8 lg:-top-6 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11 lg:text-[120px]"
+          className="absolute -left-3 -top-4 xl:-left-8 xl:-top-6 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11 lg:text-[120px]"
         >
           {"<"}
         </motion.div>
         <motion.div
           animate={{ x: [-8, 0, -8] }}
           transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-          className="absolute -left-12 -top-14 lg:-left-24 lg:-top-38 text-secondary text-[160px] lg:text-[320px] font-thin select-none"
+          className="absolute -left-12 -top-14 xl:-left-24 xl:-top-38 text-secondary text-[160px] lg:text-[320px] font-thin select-none"
         >
           {"{"}
         </motion.div>
         {/* Profile Image */}
-        <div className="relative w-46 h-46 lg:w-[320px] lg:h-[320px] content-end rounded-full border-2 border-secondary bg-radial from-accent from-40% to-primary to-100% shadow-xl">
+        <div className="relative w-46 h-46 xl:w-[320px] xl:h-[320px] content-end rounded-full border-2 border-secondary bg-radial from-accent from-40% to-primary to-100% shadow-xl">
           <img
             src={"/portfolio/" + "profile.png"} // <-- replace with your image path
             alt="Profile"
-            className="w-32 h-44 lg:w-[260px] lg:h-[350px] justify-self-center mt-2 object-cover"
+            className="w-32 h-44 xl:w-[260px] xl:h-[350px] justify-self-center mt-2 object-cover"
           />
         </div>
         <motion.div
           animate={{ x: [8, 0, 8] }}
           transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-                    className="absolute -right-12 -top-14 lg:-right-24 lg:-top-38 text-secondary text-[160px] lg:text-[320px] font-[20] select-none"
+                    className="absolute -right-12 -top-14 xl:-right-24 xl:-top-38 text-secondary text-[160px] lg:text-[320px] font-[20] select-none"
         >
           {"}"}
         </motion.div>
         <motion.div
           /* animate={{ scale: [1, 1.1, 1, 0.9, 1] }}
           transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }} */
-          className="absolute -right-4 lg:-right-12 bottom-0 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11 lg:text-[120px]"
+          className="absolute -right-4 xl:-right-12 bottom-0 text-secondary opacity-80 hover:opacity-100 content-end cursor-default text-6xl/11 lg:text-[120px]"
         >
           {">"}
         </motion.div>
